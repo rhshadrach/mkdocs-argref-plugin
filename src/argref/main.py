d7d23393289c3117a4ace384a260bafd1aa0286e
neatc0der <2805028+neatc0der@users.noreply.github.com>
2024-08-11 17:38:54 +0200

    ENH: Multiple parameters & avoid unwanted link corruption (#5)

--- src/argref/main.py
+++ src/argref/main.py
@@ -1,19 +1,118 @@
+import logging
 import re
 from mkdocs.plugins import BasePlugin
 from mkdocs.config import config_options
 
 
+log = logging.getLogger("mkdocs.plugins.argref")
+
+
+class MarkdownAutoLinker:
+    original_tag = "__ARGREF_ORIGINAL_TEXT__"
+    variable_pattern = re.compile(r"(<\S+?>)")
+
+    def __init__(self, markdown, reference, target_url):
+        self.markdown = markdown
+        self.reference_pattern = self._get_reference_pattern(reference)
+        self.link_replace_text = self._get_link_replace_text(target_url)
+
+    @classmethod
+    def _get_reference_pattern(cls, reference):
+        # ensure default <num> exists
+        # FIXME: remove in next version
+        if "<num>" not in reference:
+            log.warning(
+                f"the use of prefixes without variable is deprecated and support will be dropped in an upcoming release: {reference}"
+            )
+            reference = reference + "<num>"
+
+        # make all variables like <...> in reference detectable
+        reference_pattern = re.sub(cls.variable_pattern, r"(?P\1[-\\w]+)", reference)
+
+        # ensure original text is available
+        return re.compile(
+            rf"(?<![#\[/])(?P<{cls.original_tag}>" + reference_pattern + r")",
+            re.IGNORECASE,
+        )
+
+    @classmethod
+    def _get_link_replace_text(cls, target_url):
+        # define template for markdown link
+        template_for_linked_reference = rf"[<{cls.original_tag}>]({target_url})"
+
+        # make all variables like <...> in linked reference replacable
+        return re.sub(cls.variable_pattern, r"\\g\1", template_for_linked_reference)
+
+    def markdown_has_reference(self):
+        return re.search(self.reference_pattern, self.markdown) is not None
+
+    def replace_all_references(self):
+        self.markdown = re.sub(
+            self.reference_pattern, self.link_replace_text, self.markdown
+        )
+
+
+class AutoLinkWrapper:
+    link_pattern = re.compile(r"\[.+?\]\(.*?\)")
+    placeholder = "___AUTOLINK_PLACEHOLDER_{0}___"
+
+    class WrappedMarkdown:
+        def __init__(self, content):
+            self.__content = content
+
+        @property
+        def content(self):
+            return self.__content
+
+        @content.setter
+        def content(self, content):
+            self.__content = content
+
+    def __init__(self, markdown, link_filter_enabled):
+        self.wrapped_markdown = AutoLinkWrapper.WrappedMarkdown(markdown)
+        self.link_filter_enabled = link_filter_enabled
+        self.links = []
+
+    @property
+    def markdown(self):
+        return self.wrapped_markdown.content
+
+    def filter_links(self):
+        while True:
+            match = re.search(self.link_pattern, self.wrapped_markdown.content)
+            if match is None:
+                break
+
+            self.links.append(match.group(0))
+            self.wrapped_markdown.content = (
+                self.wrapped_markdown.content[: match.start()]
+                + self.placeholder.format(len(self.links))
+                + self.wrapped_markdown.content[match.end() :]
+            )
+
+    def recover_links(self):
+        while len(self.links) > 0:
+            self.wrapped_markdown.content = self.wrapped_markdown.content.replace(
+                self.placeholder.format(len(self.links)), self.links.pop()
+            )
+
+    def __enter__(self):
+        if self.link_filter_enabled:
+            self.filter_links()
+        return self.wrapped_markdown
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.link_filter_enabled:
+            self.recover_links()
+
+
 def replace_autolink_references(markdown, ref_prefix, target_url):
-    if "<num>" not in ref_prefix:
-        ref_prefix = ref_prefix + "<num>"
-    find_regex = re.compile(
-        r"(?<![#\[/])" + ref_prefix.replace(r"<num>", r"(?P<num>[-\w]+)"),
-        re.IGNORECASE,
-    )
-    linked_ref = rf"[{ref_prefix}](" + target_url + r")"
-    replace_text = linked_ref.replace(r"<num>", r"\g<num>")
-    markdown = re.sub(find_regex, replace_text, markdown)
-    return markdown
+    autolinker = MarkdownAutoLinker(markdown, ref_prefix, target_url)
+
+    if autolinker.markdown_has_reference():
+        autolinker.replace_all_references()
+
+    return autolinker.markdown
 
 
 class AutoLinkOption(config_options.OptionallyRequired):
@@ -30,14 +129,15 @@ class AutoLinkOption(config_options.OptionallyRequired):
                 raise config_options.ValidationError(
                     "Expected a 'target_url' in autolinks."
                 )
-            if "<num>" not in autolink["target_url"]:
-                raise config_options.ValidationError("Missing '<num>' in 'target_url'.")
+            variables = re.findall(MarkdownAutoLinker.variable_pattern, autolink["reference_prefix"])
+            variables.append("<num>")  # FIXME: remove in next version
+            if not any(v in autolink["target_url"] for v in variables):
+                raise config_options.ValidationError("At least one variable must be used in 'target_url'")
 
         return values
 
 
 class AutolinkReference(BasePlugin):
-
     config_scheme = (("autolinks", AutoLinkOption(required=True)),)
 
     def on_page_markdown(self, markdown, **kwargs):
@@ -50,9 +150,15 @@ class AutolinkReference(BasePlugin):
         :param kwargs: Other parameters (won't be used here)
         :return: Modified markdown
         """
-        for autolink in self.config["autolinks"]:
-            markdown = replace_autolink_references(
-                markdown, autolink["reference_prefix"], autolink["target_url"]
-            )
+        link_filter_enabled = self.config.get("filter_links", False) is True
+        wrapper = AutoLinkWrapper(markdown, link_filter_enabled)
 
-        return markdown
+        with wrapper as wrapped_markdown:
+            for autolink in self.config["autolinks"]:
+                wrapped_markdown.content = replace_autolink_references(
+                    wrapped_markdown.content,
+                    autolink["reference_prefix"],
+                    autolink["target_url"],
+                )
+
+        return wrapper.markdown
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,9 @@
 import pytest
 
+from argref.main import AutoLinkWrapper, AutoLinkOption
 from argref.main import replace_autolink_references as autolink
+
+from mkdocs.config import config_options
 
 simple_replace = [
     ("TAG-<num>", "http://gh/<num>", "TAG-123", "[TAG-123](http://gh/123)"),
@@ -30,6 +33,15 @@ simple_replace = [
     ),
 ]
 
+complex_replace = [
+    (
+        "<some>-TAG-<num>-<ver>",
+        "http://foo.bar/<some>?num=<num>&ver=<ver>",
+        "file-TAG-123-XYZ",
+        "[file-TAG-123-XYZ](http://foo.bar/file?num=123&ver=XYZ)",
+    ),
+]
+
 ignore_already_linked = [
     (
         "TAG-<num>",
@@ -42,6 +54,36 @@ ignore_already_linked = [
         "http://gh/TAG-<num>",
         "[TAG-789](http://gh/TAG-789)",
         "[TAG-789](http://gh/TAG-789)",
+    ),
+]
+
+ignore_url_paths_when_filter_activated = [
+    (
+        "TAG-<num>",
+        "http://gh/TAG-<num>",
+        "[see TAG-789](http://gh/TAG-789)",
+        "[see TAG-789](http://gh/TAG-789)",
+    ),
+    (
+        "TAG-<num>",
+        "http://gh/<num>",
+        "[Go Here](http://gh/TAG-789) [Go There](http://gh/TAG-123)",
+        "[Go Here](http://gh/TAG-789) [Go There](http://gh/TAG-123)",
+    ),
+]
+
+ignore_url_paths_when_filter_deactivated = [
+    (
+        "TAG-<num>",
+        "http://gh/TAG-<num>",
+        "[see TAG-789](http://gh/TAG-789)",
+        "[see [TAG-789](http://gh/TAG-789)](http://gh/TAG-789)",
+    ),
+    (
+        "TAG-<num>",
+        "http://gh/TAG-<num>",
+        "[Go Here](http://gh/abcTAG-789) [Go There](http://gh/?blub=TAG-123)",
+        "[Go Here](http://gh/abc[TAG-789](http://gh/TAG-789)) [Go There](http://gh/?blub=[TAG-123](http://gh/TAG-123))",
     ),
 ]
 
@@ -59,26 +101,121 @@ ignore_ref_links = [
 ]
 
 
+def test_deprecated_warning(caplog):
+    test_input = "GH-123"
+    ref_prefix = "GH-"
+    target_url = "http://gh/<num>"
+    autolink(test_input, ref_prefix, target_url)
+    assert (
+        "the use of prefixes without variable is deprecated and support will be dropped in an upcoming release: GH-"
+        in caplog.text
+    )
+
+
+def test_validation_of_missing_variable_in_target_url():
+    values = [
+        {
+            "reference_prefix": "GH-",
+            "target_url": "http://gh/TAG-",
+        },
+    ]
+    with pytest.raises(config_options.ValidationError) as exc_info:
+        AutoLinkOption().run_validation(values)
+    assert exc_info.value.args[0] == "At least one variable must be used in 'target_url'"
+
+
+def test_validation_of_at_least_one_variable_in_prefix_found_in_target_url():
+    values = [
+        {
+            "reference_prefix": "GH-<num>",
+            "target_url": "http://gh/TAG-<num>",
+        },
+    ]
+    AutoLinkOption().run_validation(values)
+
+
+def test_wrapper_with_enabled_link_filter():
+    test_input = "[123](abc) [456](def) [789](ghi)"
+    wrapper = AutoLinkWrapper(test_input, True)
+    with wrapper as wrapped_markdown:
+        assert (
+            wrapped_markdown.content
+            == "___AUTOLINK_PLACEHOLDER_1___ ___AUTOLINK_PLACEHOLDER_2___ ___AUTOLINK_PLACEHOLDER_3___"
+        )
+
+    assert wrapped_markdown.content == test_input
+
+
+def test_wrapper_with_disabled_link_filter():
+    test_input = "[123](abc) [456](def) [789](ghi)"
+    wrapper = AutoLinkWrapper(test_input, False)
+    with wrapper as wrapped_markdown:
+        assert wrapped_markdown.content == test_input
+
+
 @pytest.mark.parametrize(
     "ref_prefix, target_url, test_input, expected",
-    simple_replace + ignore_already_linked + ignore_ref_links,
+    simple_replace + complex_replace + ignore_already_linked + ignore_ref_links,
 )
-def test_parser(ref_prefix, target_url, test_input, expected):
-    assert autolink(test_input, ref_prefix, target_url) == expected
+@pytest.mark.parametrize("filter_links", (True, False))
+def test_parser(ref_prefix, target_url, test_input, expected, filter_links):
+    wrapper = AutoLinkWrapper(test_input, filter_links)
+    with wrapper as wrapped_mackdown:
+        wrapped_mackdown.content = autolink(
+            wrapped_mackdown.content, ref_prefix, target_url
+        )
+    assert wrapper.markdown == expected
+
+
+@pytest.mark.parametrize(
+    "ref_prefix, target_url, test_input, expected",
+    ignore_url_paths_when_filter_activated,
+)
+def test_activated_link_filter(ref_prefix, target_url, test_input, expected):
+    wrapper = AutoLinkWrapper(test_input, True)
+    with wrapper as wrapped_mackdown:
+        wrapped_mackdown.content = autolink(
+            wrapped_mackdown.content, ref_prefix, target_url
+        )
+    assert wrapper.markdown == expected
+
+
+@pytest.mark.parametrize(
+    "ref_prefix, target_url, test_input, expected",
+    ignore_url_paths_when_filter_deactivated,
+)
+def test_deactivated_link_filter(ref_prefix, target_url, test_input, expected):
+    wrapper = AutoLinkWrapper(test_input, False)
+    with wrapper as wrapped_mackdown:
+        wrapped_mackdown.content = autolink(
+            wrapped_mackdown.content, ref_prefix, target_url
+        )
+    assert wrapper.markdown == expected
 
 
 # This test address #5. It currently only checks for '#' before the link
-def test_with_attr_list():
+@pytest.mark.parametrize("filter_links", (True, False))
+def test_with_attr_list(filter_links):
     text = "## Feature 1 { #F-001 .class-feature }"
     ref_prefix = "F-<num>"
     target_url = "http://gh/<num>"
-    assert autolink(text, ref_prefix, target_url) == text
+    wrapper = AutoLinkWrapper(text, False)
+    with wrapper as wrapped_mackdown:
+        wrapped_mackdown.content = autolink(
+            wrapped_mackdown.content, ref_prefix, target_url
+        )
+    assert wrapper.markdown == text
 
 
-def test_multi_replace():
+@pytest.mark.parametrize("filter_links", (True, False))
+def test_multi_replace(filter_links):
     ref_prefix = "TAG-<num>"
     target_url = "http://gh/<num>"
     markdown = "TAG-1 TAG-1 TAG-1"
     expected = "[TAG-1](http://gh/1) [TAG-1](http://gh/1) [TAG-1](http://gh/1)"
-    result = autolink(markdown, ref_prefix, target_url)
-    assert result == expected
+    wrapper = AutoLinkWrapper(markdown, False)
+    with wrapper as wrapped_mackdown:
+        wrapped_mackdown.content = autolink(
+            wrapped_mackdown.content, ref_prefix, target_url
+        )
+    assert wrapper.markdown == expected
